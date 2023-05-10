058d0a2ffdc37571f5d80c8ee6d1868957e3aaa8
Jhovany Thedereo <jhovany11.jhovany11@gmail.com>
2023-05-11 00:18:32 +0700

    Menambahkan Terms of Services

--- lib/myacc.dart
+++ lib/myacc.dart
@@ -12,7 +12,7 @@ class _MyWidgetState extends State<MyAccInfo> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(""),
+        title: Text("sncjcjevnjb"),
       ),
       body: SingleChildScrollView(
         child: Column(
